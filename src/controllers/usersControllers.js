b001793c6a97f38547aae193b220715757599bb7
Wene Alves de Oliveira <weneplay5@gmail.com>
2020-12-31 11:24:47 -0300

    deleted all

--- src/controllers/usersControllers.js
+++
@@ -1,9 +0,0 @@
-const Users = require('../models/UsersModel');
-
-module.exports = (req, res) =>{
-   // res.locals.name
-   // req.flash('info',`Olá ${req.session.user.name}`);
-   // console.log(req.session.user);
-   // console.log(req.flash('info'));
-   res.render('index',{users: req.session.user});
-}

a6f00f44cb97c07e5360352c979c75354d588082
Wene Alves de Oliveira <weneplay5@gmail.com>
2020-12-31 11:22:18 -0300

    validate foms resgister

--- frontend/index.js
+++ frontend/index.js
@@ -1,3 +1,21 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import './assets/css/styles.css';
+import Register from './class/Register';
+
+let btnRegister = document.querySelector('#register');
+const forms = document.querySelectorAll('form');
+
+if(forms.length > 0){
+    if(btnRegister){
+        btnRegister.addEventListener('click',function(event){
+            event.preventDefault();
+            const submitRegister = Register('#emailCreater','#passwordCreater','#passwordCreaterRepite');
+            if(submitRegister){
+               forms[0].submit();
+            }
+        });
+    }
+}
+
+

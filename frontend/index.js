d881346d1078e62e25101c93b6248125782187ec
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-01 11:03:40 -0300

    add new button

--- frontend/index.js
+++ frontend/index.js
@@ -2,20 +2,36 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import './assets/css/styles.css';
 import Register from './class/Register';
+import Login from './class/Login';
 
 let btnRegister = document.querySelector('#register');
-const forms = document.querySelectorAll('form');
+let btnLogin = document.querySelector('#account');
+const formRegister = document.querySelector('#Register');
+const formLogin = document.querySelector('#Login');
 
-if(forms.length > 0){
+if(formRegister && formLogin){
     if(btnRegister){
         btnRegister.addEventListener('click',function(event){
             event.preventDefault();
             const submitRegister = Register('#emailCreater','#passwordCreater','#passwordCreaterRepite');
             if(submitRegister){
-               forms[0].submit();
+               formRegister.submit();
             }
         });
     }
+
+    if(btnLogin){
+        btnLogin.addEventListener('click',function(event){
+            event.preventDefault();
+            const submitLogin = Login('#emailAccount','#passwordAccount');
+            if(submitLogin){
+                formLogin.submit();
+            }
+        })
+    }
+
 }
+
+
 
 

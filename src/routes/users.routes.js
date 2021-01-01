b85854aa6ca83a13d752d0ca1d2291edcce445e2
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-01 18:14:56 -0300

    routes of users

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const routes = express.Router();
 const indexControllers = require('../controllers/indexControllers');
-const loginControllers = require('../controllers/loginControllers');
 
-// routes index
-routes.get('/',indexControllers);
 
-// routes index
-routes.get('/login',loginControllers);
+// routes users
+routes.get('/',indexControllers.index);
+
+
+
 
 module.exports = routes;

605ce04d5abfe29658e554702c6e05e1ba2ecafb
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-01 18:13:46 -0300

    routes of login

--- src/controllers/loginControllers.js
+++ src/controllers/loginControllers.js
@@ -1,3 +1,9 @@
-module.exports = (req, res) =>{
+const Login = require('../models/LoginModel');
+exports.login = (req, res) =>{
     res.render('login');
+}
+
+exports.register = (req, res) =>{
+    const login = new Login(req.body);
+    res.json({email:login.register()});
 }

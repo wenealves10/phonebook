9b208619692dd1e7512f0f928302dba6787d38b0
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-01 18:11:14 -0300

    delete all the  codes

--- frontend/index.js
+++ frontend/index.js
@@ -1,36 +1,7 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import './assets/css/styles.css';
-import Register from './class/Register';
-import Login from './class/Login';
 
-let btnRegister = document.querySelector('#register');
-let btnLogin = document.querySelector('#account');
-const formRegister = document.querySelector('#Register');
-const formLogin = document.querySelector('#Login');
-
-if(formRegister && formLogin){
-    if(btnRegister){
-        btnRegister.addEventListener('click',function(event){
-            event.preventDefault();
-            const submitRegister = Register('#emailCreater','#passwordCreater','#passwordCreaterRepite');
-            if(submitRegister){
-               formRegister.submit();
-            }
-        });
-    }
-
-    if(btnLogin){
-        btnLogin.addEventListener('click',function(event){
-            event.preventDefault();
-            const submitLogin = Login('#emailAccount','#passwordAccount');
-            if(submitLogin){
-                formLogin.submit();
-            }
-        })
-    }
-
-}
 
 
 

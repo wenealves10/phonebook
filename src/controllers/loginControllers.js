586ff5939736c46be0d5ff191a89b3f4d20cbdf7
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-02 11:02:34 -0300

    class of creation users login

--- src/controllers/loginControllers.js
+++ src/controllers/loginControllers.js
@@ -1,9 +1,60 @@
 const Login = require('../models/LoginModel');
-exports.login = (req, res) =>{
-    res.render('login');
+exports.login = (req, res) => {
+    if(req.session.user){
+        req.flash('errors',`Você precisa sair da conta ${req.session.user.email}`);
+        return res.redirect('/');
+    }
+    return res.render('login');
 }
 
-exports.register = (req, res) =>{
+exports.register = async (req, res) => {
+   try {
     const login = new Login(req.body);
-    res.json({email:login.register()});
+
+    await login.register();
+    if (login.errors.length > 0) {
+        req.flash('errors', login.errors);
+        req.session.save(() => {
+            return res.redirect('/login');
+        });
+        return;
+    }
+    req.flash('success','Usuário cadastrado com sucesso!');
+    req.session.save(() =>{
+        return res.redirect('/login');
+    });
+   } catch (error) {
+       console.log(error);
+       return res.render('ERROR');
+   }
+}
+
+exports.account = async (req, res) =>{
+    try {
+        const login = new Login(req.body);
+    
+        await login.account();
+        if (login.errors.length > 0) {
+            req.flash('errors', login.errors);
+            req.session.save(() => {
+                return res.redirect('/login');
+            });
+            return;
+        }
+      
+        req.flash('success','Usuário Logado com sucesso!!!');
+        req.session.user = login.user;
+        req.session.save(() =>{
+            return res.redirect('/');
+        });
+
+       } catch (error) {
+           console.log(error);
+           return res.render('ERROR');
+       }
+}
+
+exports.logout = (req, res) =>{
+    req.session.destroy();
+    res.redirect('/');
 }

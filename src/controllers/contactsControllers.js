beda3a3e2a2827b6af72579975cc1dddcf8006d6
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-02 19:41:55 -0300

    added news routes

--- src/controllers/contactsControllers.js
+++ src/controllers/contactsControllers.js
@@ -1,5 +1,9 @@
 const Contacts = require('../models/ContactsModel');
 
 exports.index = (req, res) => {
-    res.send('Contacts');
+    res.render('contacts');
+}
+
+exports.register = async (req, res) =>{
+    res.json({contacts:req.body});
 }

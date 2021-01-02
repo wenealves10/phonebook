53a15b686fc02eea726a66167729bc54e7e8fe69
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-02 19:43:52 -0300

    modifying the schemas

--- src/models/ContactsModel.js
+++ src/models/ContactsModel.js
@@ -2,13 +2,21 @@ const mongoose = require('mongoose');
 
 const ContactsSchema = new mongoose.Schema({
     name: {type: String, required: true},
-    email: {type: String, required: false},
-    phone: {type: String, required: false}
+    email: {type: String, required: false, default: ''},
+    phone: {type: String, required: false, default: ''},
+    date: {type: Date, required: false, default: Date.now()}
 });
 
 const ContactsModel = mongoose.model('Contacts',ContactsSchema);
 
 class Contacts{
+    
+    constructor(body){
+        this.body = body;
+        this.erros = [];
+        this.contacts = null;
+    }
+
 
 }
 

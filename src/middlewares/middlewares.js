4fe43494a6f04484161799a8edd611ced37ab738
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-02 11:03:03 -0300

    middlewares of errors and success

--- src/middlewares/middlewares.js
+++ src/middlewares/middlewares.js
@@ -6,6 +6,9 @@ exports.csurfError = (err, req, res, next) =>{
 }
 
 exports.csurfMiddleware = (req, res, next) => {
+    res.locals.errors = req.flash('errors');
+    res.locals.success = req.flash('success');
+    res.locals.user = req.session.user;
     res.locals.csrfToken = req.csrfToken();
     next();
 }

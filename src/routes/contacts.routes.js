3fe8b1ac353066b55c4fc4e8b60878c602cf4eee
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-02 13:59:13 -0300

    added the authentication

--- src/routes/contacts.routes.js
+++ src/routes/contacts.routes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const routes = express.Router();
 const contactsControllers = require('../controllers/contactsControllers');
+const { auth } = require('../middlewares/middlewares');
 
-routes.get('/contacts',contactsControllers.index);
+routes.get('/contacts',auth,contactsControllers.index);
 
 module.exports = routes;

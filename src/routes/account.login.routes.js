bd3600b2a68a19372ff94bfc49b8516cec25477f
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-02 11:03:46 -0300

    routes of users login

--- src/routes/account.login.routes.js
+++ src/routes/account.login.routes.js
@@ -4,6 +4,8 @@ const loginControllers = require('../controllers/loginControllers');
 
 // routes of login
 routes.get('/login',loginControllers.login);
+routes.get('/logout',loginControllers.logout);
 routes.post('/login/register',loginControllers.register);
+routes.post('/login/account',loginControllers.account);
 
 module.exports = routes;

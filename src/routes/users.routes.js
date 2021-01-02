197a207931cec3fd48c64526f69387289d2f966d
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-02 13:46:43 -0300

    rename for home.routes.js

--- src/routes/users.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const routes = express.Router();
-const indexControllers = require('../controllers/indexControllers');
-
-
-// routes users
-routes.get('/',indexControllers.index);
-module.exports = routes;

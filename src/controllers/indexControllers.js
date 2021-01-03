f16c4a9f8062f334dbaed978a0a78fc7f2eda755
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-02 21:19:34 -0300

    listing of contacts

--- src/controllers/indexControllers.js
+++ src/controllers/indexControllers.js
@@ -1,3 +1,22 @@
-exports.index = (req, res) =>{
-   return res.render('index');
+const Contacts = require('../models/ContactsModel');
+exports.index =  async (req, res) =>{
+   try{
+      let contacts = await Contacts.contacts(req.session.user);
+      const erros = Contacts.errors();
+      if(erros.length > 0){
+         contacts = [];
+         req.flash('errors',errors);
+         req.session.save(() => {
+             return res.render('index',{contacts});
+         });
+         return;
+      }
+        req.session.save(() =>{
+         return res.render('index',{contacts});
+        });
+      
+   }catch(err){
+      console.log(err);
+      res.render('ERROR');
+   }
 }

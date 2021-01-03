e31508ff8988a9980aeca7da6e82fbabcf4896aa
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-02 21:19:07 -0300

    account of contacts

--- src/controllers/contactsControllers.js
+++ src/controllers/contactsControllers.js
@@ -4,6 +4,24 @@ exports.index = (req, res) => {
     res.render('contacts');
 }
 
-exports.register = async (req, res) =>{
-    res.json({contacts:req.body});
+exports.register = async (req, res) => {
+    try {
+        const contacts = new Contacts(req.body,req.session.user);
+        await contacts.register();
+        if (contacts.errors.length > 0) {
+            req.flash('errors', contacts.errors);
+            req.session.save(() => {
+                return res.redirect('/contacts');
+            });
+            return;
+        }
+        req.flash('success','Sucesso ao Cadastra!!!');
+        req.session.save(() =>{
+            return res.redirect('/contacts');
+        });
+    }catch(error){
+        console.log(error);
+        return res.render('ERROR');
+    }
+
 }

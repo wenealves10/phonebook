c7841bbbb074281851ffee12a1adf192864fabee
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-03 11:30:09 -0300

    route update

--- src/routes/contacts.routes.js
+++ src/routes/contacts.routes.js
@@ -6,5 +6,6 @@ const { auth } = require('../middlewares/middlewares');
 routes.get('/contacts',auth,contactsControllers.index);
 routes.post('/contacts/register',auth,contactsControllers.register);
 routes.get('/contacts/:id',auth,contactsControllers.contacts);
+routes.post('/contacts/:id',auth,contactsControllers.update);
 
 module.exports = routes;

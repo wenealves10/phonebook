de9c3d9d0f23522ce9b1233e03a5ee8f56f0c7e9
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-03 20:07:27 -0300

    imports of methods of validation

--- frontend/index.js
+++ frontend/index.js
@@ -1,6 +1,18 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import './assets/css/styles.css';
+import Login from './assets/classes/Login';
+import Register from './assets/classes/Register';
+import Contacts from './assets/classes/Contacts';
+import ValTel from './assets/classes/ValTel';
+
+const login = new Login('#Login');
+login.validate();
+const register = new Register('#Register');
+register.validate();
+const contacts = new Contacts('#Contacts');
+contacts.validate();
+ValTel.init('input[type=tel]');
 
 
 

0458d45752c15fb0bbcef80c758b218f6731843e
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-02 21:31:25 -0300

    removed

--- src/controllers/indexControllers.js
+++ src/controllers/indexControllers.js
@@ -2,15 +2,6 @@ const Contacts = require('../models/ContactsModel');
 exports.index =  async (req, res) =>{
    try{
       let contacts = await Contacts.contacts(req.session.user);
-      const erros = Contacts.errors();
-      if(erros.length > 0){
-         contacts = [];
-         req.flash('errors',errors);
-         req.session.save(() => {
-             return res.render('index',{contacts});
-         });
-         return;
-      }
         req.session.save(() =>{
          return res.render('index',{contacts});
         });

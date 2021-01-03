9f0ae152e88bcd934f3d2c0ab2deb1e0e6f222db
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-03 20:08:21 -0300

    validate of phone contacts

---
+++ frontend/assets/classes/ValTel.js
@@ -0,0 +1,40 @@
+export default class ValTel{
+   
+    static init(input) {
+       let  inputTel = document.querySelector(input);
+
+        if (!inputTel) return;
+        inputTel.addEventListener('keypress', function (event) {
+            mask(this, phone);
+        })
+
+        inputTel.addEventListener('blur', function (event) {
+            mask(this, phone);
+        })
+
+        function mask(o, f) {
+            setTimeout(function () {
+                var v = phone(o.value);
+                if (v != o.value) {
+                    o.value = v;
+                }
+            }, 1);
+        }
+
+        function phone(v) {
+            var r = v.replace(/\D/g, "");
+            r = r.replace(/^0/, "");
+            if (r.length > 10) {
+                r = r.replace(/^(\d\d)(\d{5})(\d{4}).*/, "($1) $2-$3");
+            } else if (r.length > 5) {
+                r = r.replace(/^(\d\d)(\d{4})(\d{0,4}).*/, "($1) $2-$3");
+            } else if (r.length > 2) {
+                r = r.replace(/^(\d\d)(\d{0,5})/, "($1) $2");
+            } else {
+                r = r.replace(/^(\d*)/, "($1");
+            }
+            return r;
+        }
+    }
+
+}

d26780a43656a2c0781be0b5a21150a22e2c4f5b
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-02 21:20:12 -0300

    Model of contacts

--- src/models/ContactsModel.js
+++ src/models/ContactsModel.js
@@ -1,23 +1,86 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const ContactsSchema = new mongoose.Schema({
-    name: {type: String, required: true},
-    email: {type: String, required: false, default: ''},
-    phone: {type: String, required: false, default: ''},
-    date: {type: Date, required: false, default: Date.now()}
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: false,
+        default: ''
+    },
+    phone: {
+        type: String,
+        required: false,
+        default: ''
+    },
+    user_email: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
 });
 
 const ContactsModel = mongoose.model('Contacts',ContactsSchema);
 
 class Contacts{
     
-    constructor(body){
+    constructor(body, user){
+        this.user = user;
         this.body = body;
-        this.erros = [];
+        this.errors = [];
         this.contacts = null;
     }
 
+    static async contacts(user){
+        let contacts = [];
+        if(!user) return  contacts;
+         contacts = await ContactsModel.find({user_email: user.email});
+        if(!contacts.length > 0) {
+            this.errors.push('Nenhum Contatos Cadastro...');
+            return;
+        }
+        return contacts;
+    }
 
+    static errors(){
+        return this.errors;
+    }
+    
+    async register(){
+        this.validate();
+        if(this.errors.length > 0) return;
+        this.contacts = await ContactsModel.create(this.body);
+    }
+
+    validate(){
+        this.clearUp();
+        if(this.body.email && !validator.default.isEmail(this.body.email)){
+            this.errors.push('E-mail inválido!!');
+        }
+        if(!this.body.name) this.errors.push('É necessário o nome!!');
+        if(!this.body.email && !this.body.phone) this.errors.push('É necessário pelo um contato!! E-mail ou Telefone');
+    } 
+
+    clearUp(){
+        for(const key in this.body){
+            if(typeof this.body[key] !== 'string'){
+                this.body = '';
+            }
+        }
+
+        this.body = {
+            name:this.body.name,
+            email:this.body.email,
+            phone:this.body.phone,
+            user_email: this.user.email
+        }
+    }
 }
 
 module.exports = Contacts;

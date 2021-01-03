cfe91a7063d4f3f3c883eb421f165d7ce58055dd
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-03 12:17:45 -0300

    route deletion of contact

--- src/routes/contacts.routes.js
+++ src/routes/contacts.routes.js
@@ -4,8 +4,9 @@ const contactsControllers = require('../controllers/contactsControllers');
 const { auth } = require('../middlewares/middlewares');
 
 routes.get('/contacts',auth,contactsControllers.index);
-routes.post('/contacts/register',auth,contactsControllers.register);
 routes.get('/contacts/:id',auth,contactsControllers.contacts);
+routes.get('/contacts/delete/:id',auth,contactsControllers.deletes);
+routes.post('/contacts/register',auth,contactsControllers.register);
 routes.post('/contacts/:id',auth,contactsControllers.update);
 
 module.exports = routes;

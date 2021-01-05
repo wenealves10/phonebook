7e715b1d23a3f76caf5bb71003149dd39ed58172
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-05 17:13:20 -0300

    adding the use strtic

--- src/app/app.js
+++ src/app/app.js
@@ -50,7 +50,8 @@ const sessionOptions = session({
     saveUninitialized: false,
     cookie:{
         maxAge: (1000 * 60 * 60 * 24 * 7),
-        httpOnly:true
+        httpOnly:true,
+	sameSite: 'strict'
     }
 });
 app.use(sessionOptions);
